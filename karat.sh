866ac444e6ca0eac9265c383d214f36d393d848f
konijntjesbroek <proticon@gmail.com>
2020-12-06 18:57:52 -0600

    adding checks for bc

--- karat.sh
+++ karat.sh
@@ -8,6 +8,16 @@
 #   this will print out the variables in the current color scheme. If no scheme
 #   it will create a default from the dircolor db.
 #
+
+function check_bc () {
+    local sys_check=`echo '1 + 1' |bc 2>/dev/null`
+    if [ ! $sys_check ]; then
+        echo 'bc not installed'
+        exit 1
+    fi
+
+}
+
 function get_color (){
 	local -n result=$1 
 	read -p "Input a color hexmap (RR:GG:BB) q to quit: "  result
@@ -19,6 +29,9 @@ function split_color (){
 	local val=$(echo "obase=10; ibase=16; `echo ${RGB^^}|cut -d: -f${c}`" | bc)
 	echo ${val}
 }
+
+echo 'checking dependencies. . . '
+check_bc
 
 get_color RGB
 while [ $RGB != 'q' ]; do
