db6ac5f85d81532f7471477b494d894809eb7e1e
konijntjesbroek <proticon@gmail.com>
2020-12-07 23:52:17 -0600

    Initial dump of dircolors db

--- karat.sh
+++ karat.sh
@@ -10,14 +10,22 @@
 #
 
 function check_bc () {
-    local sys_check=`echo '1 + 1' |bc 2>/dev/null`
-    if [ ! $sys_check ]; then
+    local sys_check=`which bc 2>/dev/null`
+    if [ -z  ${sys_check} ]; then
         echo 'bc not installed'
         exit 1
     fi
 
 }
 
+function check_dircolors () {
+    local sys_check=`which dircolors 2>/dev/null`
+    if [ -z  ${sys_check} ]; then
+        echo 'dircolors not installed'
+        exit 1
+    fi
+
+}
 function get_color (){
 	local -n result=$1 
 	read -p "Input a color hexmap (RR:GG:BB) q to quit: "  result
@@ -32,8 +40,10 @@ function split_color (){
 
 echo 'checking dependencies. . . '
 check_bc
+check_dircolors
 
 get_color RGB
+
 while [ $RGB != 'q' ]; do
 	red=$(split_color $RGB 1)
 	green=$(split_color $RGB 2)
